ba55b418b86c02e32fb91a1b8c3ece8ffa06c68f
ilkeratabay <atabayilker@gmail.com>
2019-04-30 02:25:44 +0300

    View for HomeController Added.

--- KalamazoDefteri/Controllers/HomeController.cs
+++ KalamazoDefteri/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace KalamazoDefteri.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return Content("HelloWorld!");
+            return View();
         }
     }
 }
